13ba70f4a38321c601092c36c49d4c50bb853151
Sankeerthana Palvancha <sankeerthanapalvancha@gmail.com>
2025-07-24 06:49:17 +0000

    date filter

--- app/project1/webapp/controller/View1.controller.js
+++ app/project1/webapp/controller/View1.controller.js
@@ -12,6 +12,37 @@ sap.ui.define([
             });
 
             this.getView().setModel(oModel);
-        }
+        },
+        onDateFilterChange: function () {
+            var oTable = this.byId("resignationTable");
+            var oBinding = oTable.getBinding("items");
+        
+            var oStartDate = this.byId("startDatePicker").getDateValue();
+            var oEndDate = this.byId("endDatePicker").getDateValue();
+        
+            var aFilters = [];
+        
+            var formatDate = function (date) {
+                if (!date) return null;
+                var yyyy = date.getFullYear();
+                var mm = String(date.getMonth() + 1).padStart(2, '0');
+                var dd = String(date.getDate()).padStart(2, '0');
+                return `${yyyy}-${mm}-${dd}`;
+            };
+        
+            var sStartDate = formatDate(oStartDate);
+            var sEndDate = formatDate(oEndDate);
+        
+            if (sStartDate) {
+                aFilters.push(new sap.ui.model.Filter("Date", sap.ui.model.FilterOperator.GE, sStartDate));
+            }
+        
+            if (sEndDate) {
+                aFilters.push(new sap.ui.model.Filter("Date", sap.ui.model.FilterOperator.LE, sEndDate));
+            }
+        
+            oBinding.filter(aFilters);
+        },
+        
     });
 });
