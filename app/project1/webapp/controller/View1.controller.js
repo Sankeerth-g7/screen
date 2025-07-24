916bc1ac9bfe3914079975886f4908bfb95fcd82
SANKEERTH ERRA <sankeertherra01@gmail.com>
2025-07-24 06:25:22 +0000

    basic table

--- app/project1/webapp/controller/View1.controller.js
+++ app/project1/webapp/controller/View1.controller.js
@@ -1,10 +1,17 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/odata/v4/ODataModel"
+], function (Controller, ODataModel) {
     "use strict";
 
     return Controller.extend("project1.controller.View1", {
-        onInit() {
+        onInit: function () {
+            const oModel = new ODataModel({
+                serviceUrl: "/odata/v4/my/",
+                synchronizationMode: "None"
+            });
+
+            this.getView().setModel(oModel);
         }
     });
 });
